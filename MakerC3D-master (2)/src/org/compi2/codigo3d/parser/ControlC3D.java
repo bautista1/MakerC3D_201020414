1f43fa37c9594d4779c8e639cff4e11e630a6fcf
bautista1 <alberthbau@gmail.com>
2017-03-17 23:05:05 -0600

    HOJA DE TRABAJO 3 - CORTOCIRCUITO LISTO Nombre:Alberth Isai Godinez Bautista Carnet:201020414

--- MakerC3D-master (2)/src/org/compi2/codigo3d/parser/ControlC3D.java
+++ MakerC3D-master (2)/src/org/compi2/codigo3d/parser/ControlC3D.java
@@ -8,7 +8,11 @@ public class ControlC3D {
     
     private static int temp = 0;
     private static String c3d = "";
-    
+    private static int contEtiqueta=0;
+    private static String etiqueta="";
+    private static String eV="";
+    private static String eF="";
+    private static int pre=0;
     /**
      * Reinicia las variables estáticas relacionadas con la generación del
      * código de tres direcciones.
@@ -16,6 +20,11 @@ public class ControlC3D {
     public static void reiniciar(){
         temp = 0;
         c3d = "";
+        contEtiqueta=0;
+        etiqueta="";
+        eV="";
+        eF="";
+        pre=0;
     }
     
     /**
@@ -42,5 +51,41 @@ public class ControlC3D {
     public static String getC3D(){
         return c3d;
     }
+    
+    public static String genEtiqueta(){
+        return "L"+contEtiqueta++;
+    }
+    
+    public static void almacenarEv(String ev){
+        eV+=","+ev;
+    }
+    public static void almacenarEf(String ef){
+        eF+=","+ef;
+    }
+    
+    public static void formatearEv(){
+        eV="";
+    }
+    
+    public static void formatearEf(){
+        eF="";
+    }
+    
+    public static String getEv(){
+        return eV;
+    }
+    
+    public static String getEf(){
+        return eF;
+    }
+
+    public static int getPre() {
+        return pre;
+    }
+
+    public static void setPre(int pre) {
+        ControlC3D.pre = pre;
+    }
+    
     
 }
